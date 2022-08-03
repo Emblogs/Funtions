dca10fe5c00d7700df5e82d2b51c19276626c46a
Emblogs <Emzy45cool@gmail.com>
2022-08-03 10:01:57 -0700

    first commit

---
+++ Function.js
@@ -0,0 +1,74 @@
+let threeraiseto2=Math.pow(3,2)
+console.log("3 raise to power of 2 is:",threeraiseto2)
+
+function square() {
+    console.log(3**3)
+}
+for(let count=1; count<5; count++){
+    square()
+}
+
+function name() {
+    console.log("Emmmanuel")
+}
+name()
+
+function multiply() {
+    let sad=2
+    let des=3
+let product=sad*des
+let message="2 times 3 is "+product
+console.log(message)
+}
+multiply()
+
+function addition(){
+    let x=3
+    let y=4
+let add=x+y
+let display="3 plus 4 is "+add
+console.log(display)
+}
+addition()
+
+function division(){
+    let w=12
+    let x=4
+let divide=w/x
+let vision="12 divided by 4 is "+divide
+console.log(vision)
+}
+division()
+
+function subtraction(){
+    let red=10
+    let ped=5
+let subtract=red-ped
+let dash="5 subtracted from 5 is "+subtract
+console.log(dash)
+}
+subtraction()
+
+function multiplication(a,b){
+    let product=a*b
+    console.log(product)
+    return product
+}
+let house=multiplication(8,3)
+console.log(house)
+
+function addition(a,b){
+    let pro=a+b
+    return pro
+
+}
+
+let mouse=addition(7,house)
+console.log(mouse)
+
+function subtraction(a,b){
+    let reed=a-b
+    return reed
+}
+let web=subtraction(mouse,2)
+console.log(web)
